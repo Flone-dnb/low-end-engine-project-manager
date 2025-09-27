04da6a533e9fb5d242a52b7f1175e896db822c21
Alexander Tretyakov <flonednb@gmail.com>
2025-09-27 19:57:02 +0300

    Format initial game code.

--- game_lib/src/MyGameInstance.cpp
+++ game_lib/src/MyGameInstance.cpp
@@ -5,27 +5,26 @@
 #include "render/FontManager.h"
 #include "render/Renderer.h"
 
-MyGameInstance::MyGameInstance(Window *pWindow) : GameInstance(pWindow) {}
+MyGameInstance::MyGameInstance(Window* pWindow) : GameInstance(pWindow) {}
 
 void MyGameInstance::registerGameTypes() {
-  // Register your custom types in the reflection database here.
-  // You should do it in this function because editor_lib will call this
-  // function in its onGameStarted to allow you to use your game types in the
-  // editor.
-  //
-  // For example:
-  // ReflectedVariableType::registerType(MyNode::getTypeGuidStatic(),
-  // MyNode::getReflectionInfo());
+    // Register your custom types in the reflection database here.
+    // You should do it in this function because editor_lib will call this
+    // function in its onGameStarted to allow you to use your game types in the
+    // editor.
+    //
+    // For example:
+    // ReflectedVariableType::registerType(MyNode::getTypeGuidStatic(),
+    // MyNode::getReflectionInfo());
 }
 
 void MyGameInstance::onGameStarted() {
-  registerGameTypes();
+    registerGameTypes();
 
-  getRenderer()->getFontManager().loadFont(
-      ProjectPaths::getPathToResDirectory(ResourceDirectory::ENGINE) / "font" /
-      "font.ttf");
+    getRenderer()->getFontManager().loadFont(
+        ProjectPaths::getPathToResDirectory(ResourceDirectory::ENGINE) / "font" / "font.ttf");
 
-  createWorld([](Node *pRootNode) {
-    // TODO
-  });
+    createWorld([](Node* pRootNode) {
+        // TODO
+    });
 }
